915a01d3d2cd9f70b2e9392737832c7608636a9f
juanpablocely21 <juanpablocely@mansuba.co>
2023-05-25 12:37:44 -0500

    Mixin

---
+++ mixin.dart
@@ -0,0 +1,28 @@
+void main(List<String> args) {
+  final delfin = new Delfin();
+  final murcielago = new Murcielago();
+  delfin.nadar();
+  murcielago.volar();
+}
+
+abstract class Animal{}
+
+abstract class Mamifero extends Animal{}
+abstract class Ave extends Animal{}
+abstract class Pez extends Animal{}
+
+abstract class Volador {
+  void volar() => print("Puedo volar");
+}
+
+abstract class Nadador {
+  void nadar() => print("Puedo nadar");
+}
+
+abstract class Caminador {
+  void caminar() => print("Puedo caminar");
+}
+
+
+class Delfin extends Mamifero with Nadador{}
+class Murcielago extends Mamifero with Volador{}
